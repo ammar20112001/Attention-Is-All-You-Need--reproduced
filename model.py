eb19f408fb8eba9174d346b678b1fa1b9ced99c0
Ammar <ammarjawed.1111@gmail.com>
2024-09-15 18:02:11 +0300

    Adding Transformers Positional Encodings Layer

--- model.py
+++ model.py
@@ -26,3 +26,35 @@ class Embeddings(nn.Module):
             e -->   (B, S, d_model)       d_model: Model's Dimension
         '''
         return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncodings(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a 2 dimensional array of Positional Encodings
+        # pe --> (S, d_model)
+        pe = [[math.sin(p/(10_000**((2*i)/self.d_model))) if i % 2 == 0 else math.cos(p/(10_000**((2*i)/self.d_model))) for i in range(self.d_model)] for p in range (self.seq_len)]
+        
+        # Convert pe list to Torch Tensor
+        pes = torch.FloatTensor(pes)
+        # Add a batch dimension to the positional encoding
+        # pe --> (1, S, d_model)
+        pe = pe.unsqueeze(0)
+
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        '''
+        Arguments:
+            x -->   (B, S, d_model)
+
+        Return:
+            PE -->  (B, S, d_model)
+        '''
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # This operation automatically broadcasts the Positional Encodings (pe) across all batches of x
+        return self.dropout(x)
