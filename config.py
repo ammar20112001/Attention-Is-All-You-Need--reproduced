eb5e6196f21ad18f64ad31448e86fe305319a33d
Ammar <ammarjawed.1111@gmail.com>
2024-09-19 19:29:53 +0300

    Updating config file

--- config.py
+++ config.py
@@ -4,4 +4,9 @@ def config():
         "lang_src": "en",
         "lang_tgt": "it",
         "datasource": 'opus_books',
+
+        'tokenizer': 'bert-base-uncased',
+        
+        'enc_max_seq_len': 512,
+        'dec_max_seq_len': 512,
     }
