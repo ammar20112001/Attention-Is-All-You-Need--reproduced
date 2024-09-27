d6fab225cfe190c816cf8aec90322a21fd4911c6
Ammar <ammarjawed.1111@gmail.com>
2024-09-27 08:20:43 +0300

    Add lr and optimizer keys to configuration file

--- config.py
+++ config.py
@@ -17,6 +17,10 @@ def configuration():
         'enc_max_seq_len': 256, # In paper: 512
         'dec_max_seq_len': 256, # In paper: 512
 
+        # Learning rate and Optimizer
+        'lr': 0.001,
+        'optimizer': 'Adam',
+
         # Transformer model architecture details
         'd_model': 256, # In paper: 512
         'heads': 4, # In paper: 8
