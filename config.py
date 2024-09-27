6a5fb6f65e4e14c3c4a1932cce6db50948144af6
Ammar <ammarjawed.1111@gmail.com>
2024-09-27 09:33:22 +0300

    Cleaning config file and adding nested dictionaries

--- config.py
+++ config.py
@@ -1,35 +1,47 @@
 def configuration():
     return {
-        "dataset": "kaitchup/opus-English-to-French",
-        "lang_src": "en",
-        "lang_tgt": "it",
-        "datasource": 'opus_books',
 
-        'batch_size': 64,
-
-        'train_frac': 0.9,
-        'test_val_frac': 0.5,
-        'train_rows': 10_000,
-        'test_val_rows': 1_000,
-
-        'tokenizer': 'bert-base-uncased',
+        # Dataset configurations
+        'dataset_configs': {
+            "dataset": "kaitchup/opus-English-to-French",
+            "lang_src": "en",
+            "lang_tgt": "it",
+            "datasource": 'opus_books',
+            # Tokenizer
+            'tokenizer': 'bert-base-uncased',
+            # Data loader configurations
+            'batch_size': 64,
+            # Data split configurations
+            'train_frac': 0.9,
+            'test_val_frac': 0.5,
+            'train_rows': 10_000, # Set value to `False` to laod full dataset
+            'test_val_rows': 1_000,
+            },
         
-        'enc_max_seq_len': 256, # In paper: 512
-        'dec_max_seq_len': 256, # In paper: 512
+        # Transformer model configurations
+        'model_configs': {
+            'd_model': 256, # In paper: 512
+            'heads': 4, # In paper: 8
+            'n_stack': 3, # In paper: 6
+            'max_seq_len': 256, # In paper: 512
+            'src_vocab_size': 40_000,
+            'tgt_vocab_size': 40_000,
+            'dropout': 0.1, # In paper: 0.1
+            'd_fc': 1024, # In paper: 2048
+            'enc_max_seq_len': 256, # In paper: 512
+            'dec_max_seq_len': 256, # In paper: 512
+        },
 
-        # Learning rate
-        'lr': 0.001,
-
-        # Transformer model architecture details
-        'd_model': 256, # In paper: 512
-        'heads': 4, # In paper: 8
-        'n_stack': 3, # In paper: 6
-        'max_seq_len': 256, # In paper: 512
-        'src_vocab_size': 40_000,
-        'tgt_vocab_size': 40_000,
-        'dropout': 0.1, # In paper: 0.1
-        'd_fc': 1024, # In paper: 2048
+        # Training configurations
+        'training_configs': {
+            # Learning rate
+            'lr': 0.001,
+            'optimizer': 'Adam', # Hard coded
+        },
 
         # W&B configs
-        'log_text_len': 15
+        'wandb_configs':{
+            'log_text_len': 15
+        }
+
     }
