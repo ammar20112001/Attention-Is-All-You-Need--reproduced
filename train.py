6499254d5fc8d9bd3b4174f42858fb1ea13c32e0
Ammar <ammarjawed.1111@gmail.com>
2024-10-07 14:36:01 +0300

    Remove LightningCLI implementation

--- train.py
+++ train.py
@@ -1,12 +1,29 @@
-from lit_model import transformerLightning
+from lit_model import transformerLightning, checkpoint_callback
 from lit_data_module import DataModuleLightning
 
-import torch
 import lightning as L
-from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.loggers import WandbLogger
+
+from config import configuration
 
 if __name__ == '__main__':
-    cli = LightningCLI(
-        model_class=transformerLightning,
-        datamodule_class=DataModuleLightning
-        )
+
+    config = configuration()
+
+    # Create WandBLogger
+    wandb_logger = WandbLogger(project="Attention-Is-All-You-Need--reproduced", log_model="all", config=config)
+
+    # Create data instance
+    dataset = DataModuleLightning(config)
+
+    # Create model instance
+    model = transformerLightning(config, logger=wandb_logger)
+
+    # Train model
+    trainer = L.Trainer(max_epochs=config['epochs'], 
+                        callbacks=[checkpoint_callback], 
+                        logger=wandb_logger)
+    # Fit model
+    trainer.fit(model=model, 
+                datamodule=dataset,
+                )
