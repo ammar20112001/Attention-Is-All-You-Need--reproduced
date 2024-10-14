3a95ed4f3cc429fa1de73a930b4b4722b2a10c34
Ammar <ammarjawed.1111@gmail.com>
2024-10-14 20:32:18 +0300

    Add Model Staging module

---
+++ stage_model.py
@@ -0,0 +1,17 @@
+# Stage a model that has been trained and stored as an artifcat on W&B
+
+# Login W&B
+# Load model from W&B
+
+# Convert to PyTorch model to torchscript for production environment
+
+# Download model to production directory
+# Download model to W&B for other uses
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
