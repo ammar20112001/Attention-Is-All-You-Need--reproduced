e2caa0ce1a4d54338c613822d9e7ebbfc5efb2af
Ammar <ammarjawed.1111@gmail.com>
2024-10-21 23:59:00 +0300

    pre-commit

--- prod/flaskapi.py
+++ prod/flaskapi.py
@@ -1,5 +1,3 @@
-import torch
-
 from flask import Flask, request, render_template
 
 from prod.LanguageTranslator import LanguageTranslator
@@ -10,12 +8,12 @@ model = LanguageTranslator()
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method=="POST":
+    if request.method == "POST":
         x = request.form.get("englishSentence")
         y = model.predict(x)
 
         return render_template("page.html", english=x, french=y)
 
-    return render_template('page.html')
+    return render_template("page.html")
