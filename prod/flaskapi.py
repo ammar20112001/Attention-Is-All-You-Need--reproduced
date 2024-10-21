c834cce07d0726c15197eb3bf52a3c9a5a27b93d
Ammar <ammarjawed.1111@gmail.com>
2024-10-21 23:43:04 +0300

    Update flask app

--- prod/flaskapi.py
+++ prod/flaskapi.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def translate():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method=="POST":
+        english_sentence = request.form.get("englishSentence")
+        print(english_sentence)
+        return render_template("page.html")
+
+    return render_template('page.html')
