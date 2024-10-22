e09478c2a434782f48638774eaa7f30a476727f7
Ammar <ammarjawed.1111@gmail.com>
2024-10-22 06:18:03 +0300

    pre-commit

--- aws_lambda/lambda/main.py
+++ aws_lambda/lambda/main.py
@@ -4,13 +4,13 @@ from LanguageTranslator import LanguageTranslator
 
 model = LanguageTranslator()
 
+
 def lambda_handler(event, context):
-    x = json.loads(event['body'])['englishSentence']
+    x = json.loads(event["body"])["englishSentence"]
 
     # Make prediction
     y = model(x)
 
     return {
-        'prediction': y,
+        "prediction": y,
     }
-    
