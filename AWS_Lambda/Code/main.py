b2c4131c8cc672979149ed72748626ba920fc7d8
Ammar <ammarjawed.1111@gmail.com>
2024-10-26 12:24:13 +0300

    pre-commit

--- AWS_Lambda/Code/main.py
+++ AWS_Lambda/Code/main.py
@@ -12,7 +12,7 @@ except Exception as e:
     print("Failed at unzip_torch", e, sep="\n")
 
 
-s3 = boto3.client('s3')
+s3 = boto3.client("s3")
 
 
 def download_model(bucket, key):
@@ -21,7 +21,7 @@ def download_model(bucket, key):
     if not os.path.exists(model_dir):
         # Download model from s3 to temp dir
         s3.Bucket(bucket).download_file(key, model_dir)
-    
+
     return model_dir
 
 
@@ -33,18 +33,12 @@ def lambda_handler(event, context):
     x = json.loads(event["body"])["englishSentence"]
 
     # Downloads model from s3 bucket and return path
-    model_dir = download_model(
-        bucket='translator-model-bucket',
-        key='model.pt'
-        )
-    
+    model_dir = download_model(bucket="translator-model-bucket", key="model.pt")
+
     # Load model
     model = load_model(model_dir)
 
     # Make prediction
     y = model(x)
 
-    return {
-        "status": 200,
-        "prediction": y
-    }
+    return {"statusCode": 200, "prediction": y}
